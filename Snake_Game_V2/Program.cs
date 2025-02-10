17519fb5938a12e5515869c0959e1de587b7648d
clmirabent <leomiracar@gmail.com>
2025-02-10 21:32:45 +0100

    all methods

---
+++ Snake_Game_V2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Snake_Game_V2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("-----WELCOME TO SNAKE GAME V2-----");
+            var game = new Game();
+            game.Run();
+        }
+        
+    }
+}
